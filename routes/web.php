bb5a0548549dc688ef23a1d1368bf56609aae500
richardviteri <richard.viteri@gmail.com>
2022-06-03 14:56:46 -0400

    para resolver merge de route web

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,14 @@ Auth::routes();
 
 Route::middleware(['auth',])->group(function () {
 
-  Route::get('/', 'HomeController@index')->name('home');
-  //Route::get('user-autocomplete', 'UserController@autocomplete');
-
   Route::resource('user', 'Admin\UserController');
   Route::get('users', 'Admin\UserController@getUser');
   Route::get('users/{user_id}/delete', 'Admin\UserController@delete');
   //Route::resource('logins', 'LoginController');
   Route::resource('permisos', 'Admin\PermisosController');
   Route::resource('roles', 'Admin\RolesController');
+  Route::resource('personal', 'Covid\PersonalController');
+  Route::resource('visitante', 'Covid\VisitanteController');
+  //Route::resource('personal', Covid\PersonalController::class);
 
 });
