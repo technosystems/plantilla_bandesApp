0c01ea5833e33b72491e13845615b48b48964c51
Maigom <gmrmai@gmail.com>
2022-06-10 13:59:09 -0500

    logo del header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,15 +23,7 @@
   </div>
   <div class="col-lg-4 col-md-4 order-1">
     <div class="row">
-      <div class="col-lg-6 col-md-12 col-6 mb-4">
-        <div class="card">
-          <div class="card-body pb-0">
-            <span class="d-block fw-semibold mb-1">Order</span>
-            <h3 class="card-title mb-1">276k</h3>
-          </div>
-          <div id="orderChart" class="mb-3"></div>
-        </div>
-      </div>
+
       <div class="col-lg-6 col-md-12 col-6 mb-4">
         <div class="card">
           <div class="card-body">
