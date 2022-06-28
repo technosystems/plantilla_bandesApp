a0cc32df69311cba40b96273d0a70053e42a980f
maigom <gmrmai@gmail.com>
2022-06-28 16:09:04 -0500

    avances en ela vista de visistantes

--- resources/views/covid/visitante/index.blade.php
+++ resources/views/covid/visitante/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 @section('title', 'Visitante')
+
 @section('content')
  <div class="page-content">
   <div class="container-fluid">
@@ -76,254 +77,254 @@
           </div>
        </div>
     </div>
-   @include('covid.visitante.modal.prueba')
+    @include('covid.visitante.modal.prueba')
     @include('covid.visitante.modal.consulta')
   </div>
-
-
 @endsection
 
 @push('styles')
  <!-- gridjs css -->
  <link rel="stylesheet" href="{{ asset('assets/vendor/libs/sweetalert2/sweetalert2.css') }}">
-
 @endpush
 
 @push('scripts')
-<script src="/assets/vendor/libs/sweetalert2/sweetalert2.js"></script>
-<script src="/assets/js/forms-selects.js"></script>
+
+  <script src="/assets/vendor/libs/sweetalert2/sweetalert2.js"></script>
+  <script src="/assets/js/forms-selects.js"></script>
+
   <script>
-    var user_id, opcion;
-    opcion = 4;
-    tablaModulos =  $('#tablaModulos').DataTable({
-       language: {
-        "decimal": "",
-        "emptyTable": "No hay información",
-        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-        "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
-        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
-        "infoPostFix": "",
-        "thousands": ",",
-        "lengthMenu": "Mostrar _MENU_ Entradas",
-        "loadingRecords": "Cargando...",
-        "processing": "Procesando...",
-        "search": "Buscar:",
-        "zeroRecords": "Sin resultados encontrados",
-        "paginate": {
-            "first": "Primero",
-            "last": "Ultimo",
-            "next": "Siguiente",
-            "previous": "Anterior"
-            },
-        "oAria": {
-          "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-          "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-        }
-        },
-        dom: 'Bfrtip',
-         responsive:false,
-         lengthChange: true,
-         buttons: [
-            'excel', 'pdf', 'print','colvis'
-        ],
-    "ajax":{
-        "url": "{{ url('visitantes') }}",
-        "method": 'GET', //usamos el metodo POST
-        "dataSrc":""
-    },
-    "columns":[
-        {"data": "id_personal"},
-        {"data": "cedula"},
-        {"data": "tx_nombres"},
-        {"data": "tx_apellidos"},
-        {"data": "id_gerencia"},
-        {"data": "id_estatus"},
+      var user_id, opcion;
+      opcion = 4;
+      tablaModulos =  $('#tablaModulos').DataTable({
+          language: {
+            "decimal": "",
+            "emptyTable": "No hay información",
+            "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+            "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+            "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ Entradas",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "search": "Buscar:",
+            "zeroRecords": "Sin resultados encontrados",
+            "paginate": {
+                "first": "Primero",
+                "last": "Ultimo",
+                "next": "Siguiente",
+                "previous": "Anterior"
+                },
+            "oAria": {
+              "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+              "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+            }
+          },
+          dom: 'Bfrtip',
+           responsive:false,
+           lengthChange: true,
+           buttons: [
+              'excel', 'pdf', 'print','colvis'
+           ],
+          "ajax":{
+          "url": "{{ url('visitantes') }}",
+          "method": 'GET', //usamos el metodo POST
+          "dataSrc":""
+          },
+          "columns":[
+          {"data": "id_personal"},
+          {"data": "cedula"},
+          {"data": "tx_nombres"},
+          {"data": "tx_apellidos"},
+          {"data": "id_gerencia"},
+          {"data": "id_estatus"},
 
-        {"defaultContent": " <div class='btn-group'><button class='btn btn-primary btn-sm btn-circle btnEditar'><i class='bx bx-detail'></i></button><button class='btn btn-success btn-sm btn-circle btnBorrar'><i class='bx bx-link-alt'></i></button></div>"}
-    ]
-});
-    var fila; //captura la fila, para editar o eliminar
-
-/************** levanta la modal para registrar los datos del resultado de la prueba covid ********/
-$(document).on("click", ".btnEditar", function(){
-    opcion = 2;//editar
-    $(".modal-title").text("Resultado de la Prueba");
-    $('#Modal1').modal('show');
-
-});
-//submit para el Alta y Actualización
-$('#main-form').submit(function(e){
-    e.preventDefault(); //evita el comportambiento normal del submit, es decir, recarga total de la página
-    name = $.trim($('#nombreusuario').val());
-    last_name = $.trim($('#apellido').val());
-    status = $.trim($('#status').val());
-    username = $.trim($('#usuario').val());
-    codigo = $.trim($('#txtCodigo').val());
-    var data = $('#main-form').serialize();
-    $('#ajax-icon').removeClass('far fa-save').addClass('fas fa-spin fa-sync-alt');
-    $.ajax({
-         url: "/user/" + user_id,
-          headers: {'X-CSRF-TOKEN': $('#main-form #_token').val()},
-          type: "PUT",
-          datatype:"json",
-          cache: false,
-          data:  data,
-        success: function (response) {
-          var json = $.parseJSON(response);
-          if(json.success){
-            $('#main-form #edit-button').removeClass('hide');
-
-            tablaModulos.ajax.reload(null, false);
-            Swal.fire({
-              title:'¡Bien hecho!',
-              text:'Datos ingresados',
-              icon:"success",
-              customClass:{confirmButton:"btn btn-primary"},
-              buttonsStyling:!1
-
-            })
-          }
-        },error: function (data) {
-          var errors = data.responseJSON;
-          $.each( errors.errors, function( key, value ) {
-            toastr.error(value);
-            return false;
-          });
-          //$('input').iCheck('enable');
-          $('#main-form input, #main-form button').removeAttr('disabled');
-          $('#ajax-icon').removeClass('fas fa-spin fa-sync-alt').addClass('far fa-save');
-        }
-     });
-    $('#ModulosEdit').modal('hide');
-});
-
- /******** levanta la modal de consulta para ver historial de pruebas covid del visitante   *****/
-  $(document).on("click", ".btnCsta", function(){
-
-      opcion = 2;//editar
-
-      $(".modal-title").text("Historial de Pruebas Covid del Visitante");
-      $('#ModalConsulta').modal('show');
-  });
-</script>
-  <script>
-
-    $('#edit-button').hide();
-     newUserForm.on('submit', function (e) {
-      var isValid = newUserForm.valid();
-      e.preventDefault();
-
-       if (isValid) {
-         var data = $('#usuarios-form').serialize();
-        //$('input').iCheck('disable');
-        //$('#usuarios-form input, #usuarios-form button').attr('disabled','true');
-        $('#ajax-icon').removeClass('far fa-save').addClass('fas fa-spin fa-sync-alt');
-
-            $.ajax({
-              url: $('#usuarios-form #_url').val(),
-              headers: {'X-CSRF-TOKEN': $('#usuarios-form #_token').val()},
-              type: 'POST',
-              cache: false,
-              data: data,
-              success: function (response) {
-                var json = $.parseJSON(response);
-                if(json.success){
-
-                  $('#usuarios-form #submit').hide();
-                  $('#usuarios-form #edit-button').attr('href', $('#usuarios-form #_url').val() + '/' + json.user_id + '/edit');
-                  $('#usuarios-form #edit-button').removeClass('hide');
-                  tablaModulos.ajax.reload(null, false);
-                  var timerInterval;
-                  Swal.fire({
-
-                       title:'¡Bien hecho!',
-                      text:'Datos ingresados',
-                      icon:"success",
-                      customClass:{confirmButton:"btn btn-primary"},
-                      buttonsStyling:!1
-                    })
-
-
-               }
-              },error: function (data) {
-                var errors = data.responseJSON;
-                $.each( errors.errors, function( key, value ) {
-                  toastr.error(value);
-                  return false;
-                });
-                //$('input').iCheck('enable');
-                $('#formModulos input, #main-form button').removeAttr('disabled');
-                $('#ajax-icon').removeClass('fas fa-spin fa-sync-alt').addClass('far fa-save');
-              }
-           });
-
-
-       }
-       else
-       {
-         return false;
-       }
-    });
-  </script>
-  <script>
-   $(document).on("click", ".btnBorrar", function(e){
-    e.preventDefault();
-    fila = $(this);
-    user_id = parseInt($(this).closest('tr').find('td:eq(0)').text()) ;
-    opcion = 3; //eliminar
-    Swal.fire({
-        title: '¿Estás seguro(a)?',
-        text: "¡Si confirmas no habrá marcha atrás!",
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: '¡Eliminar!',
-        customClass: {
-          confirmButton: 'btn btn-primary',
-          cancelButton: 'btn btn-outline-danger ms-1'
-        },
-        buttonsStyling: false
-      }).then(function (result) {
-        if (result.value) {
-        $.ajax({
-          url: "/users/"+user_id+'/delete' ,
-          type: "GET",
-          datatype:"json",
-
-          success: function() {
-              tablaModulos.row(fila.parents('tr')).remove().draw();
-               var timerInterval;
-                  Swal.fire({
-                    title: '¡Datos eliminados!',
-                    icon: 'success',
-                    timer: 1000,
-                    timerProgressBar: false,
-                    didOpen: () => {
-                      Swal.showLoading();
-                      timerInterval = setInterval(() => {
-                        const content = Swal.getHtmlContainer();
-                        if (content) {
-                          const b = content.querySelector('b');
-                          if (b) {
-                            b.textContent = Swal.getTimerLeft();
-                          }
-                        }
-                      }, 100);
-                    },
-                    willClose: () => {
-                      clearInterval(timerInterval);
-                    }
-                  }).then(result => {
-                    /* Read more about handling dismissals below */
-                    if (result.dismiss === Swal.DismissReason.timer) {
-                      console.log('I was closed by the timer');
-                    }
-                  });
-           }
-        });
-
-        }
+          {"defaultContent": " <div class='btn-group'><button class='btn btn-primary btn-sm btn-circle btnEditar'><i class='bx bx-detail'></i></button><button class='btn btn-success btn-sm btn-circle btnCsta'><i class='bx bx-link-alt'></i></button></div>"}
+          ]
       });
+      var fila; //captura la fila, para editar o eliminar
 
- });
+      /************** levanta la modal para registrar los datos del resultado de la prueba covid ********/
+      $(document).on("click", ".btnEditar", function(){
+        opcion = 2;//editar
+        $(".modal-title").text("Resultado de la Prueba");
+        $('#Modal1').modal('show');
+
+      });
+      //submit para el Alta y Actualización
+     /* $('#main-form').submit(function(e){
+        e.preventDefault(); //evita el comportambiento normal del submit, es decir, recarga total de la página
+        name = $.trim($('#nombreusuario').val());
+        last_name = $.trim($('#apellido').val());
+        status = $.trim($('#status').val());
+        username = $.trim($('#usuario').val());
+        codigo = $.trim($('#txtCodigo').val());
+        var data = $('#main-form').serialize();
+        $('#ajax-icon').removeClass('far fa-save').addClass('fas fa-spin fa-sync-alt');
+        $.ajax({
+             url: "/user/" + user_id,
+              headers: {'X-CSRF-TOKEN': $('#main-form #_token').val()},
+              type: "PUT",
+              datatype:"json",
+              cache: false,
+              data:  data,
+            success: function (response) {
+              var json = $.parseJSON(response);
+              if(json.success){
+                $('#main-form #edit-button').removeClass('hide');
+
+                tablaModulos.ajax.reload(null, false);
+                Swal.fire({
+                  title:'¡Bien hecho!',
+                  text:'Datos ingresados',
+                  icon:"success",
+                  customClass:{confirmButton:"btn btn-primary"},
+                  buttonsStyling:!1
+
+                })
+              }
+            },error: function (data) {
+              var errors = data.responseJSON;
+              $.each( errors.errors, function( key, value ) {
+                toastr.error(value);
+                return false;
+              });
+              //$('input').iCheck('enable');
+              $('#main-form input, #main-form button').removeAttr('disabled');
+              $('#ajax-icon').removeClass('fas fa-spin fa-sync-alt').addClass('far fa-save');
+            }
+         });
+        $('#ModulosEdit').modal('hide');
+      });*/
+
+      /******** levanta la modal de consulta para ver historial de pruebas covid del visitante   *****/
+      $(document).on("click", ".btnCsta", function(){
+
+          opcion = 2;//editar
+
+          $(".modal-title").text("Historial de Pruebas Covid del Visitante");
+          $('#ModalConsulta').modal('show');
+      });
+  </script>
+
+  <script>
+
+      $('#edit-button').hide();
+       newUserForm.on('submit', function (e) {
+        var isValid = newUserForm.valid();
+        e.preventDefault();
+
+         if (isValid) {
+           var data = $('#usuarios-form').serialize();
+          //$('input').iCheck('disable');
+          //$('#usuarios-form input, #usuarios-form button').attr('disabled','true');
+          $('#ajax-icon').removeClass('far fa-save').addClass('fas fa-spin fa-sync-alt');
+
+              $.ajax({
+                url: $('#usuarios-form #_url').val(),
+                headers: {'X-CSRF-TOKEN': $('#usuarios-form #_token').val()},
+                type: 'POST',
+                cache: false,
+                data: data,
+                success: function (response) {
+                  var json = $.parseJSON(response);
+                  if(json.success){
+
+                    $('#usuarios-form #submit').hide();
+                    $('#usuarios-form #edit-button').attr('href', $('#usuarios-form #_url').val() + '/' + json.user_id + '/edit');
+                    $('#usuarios-form #edit-button').removeClass('hide');
+                    tablaModulos.ajax.reload(null, false);
+                    var timerInterval;
+                    Swal.fire({
+
+                         title:'¡Bien hecho!',
+                        text:'Datos ingresados',
+                        icon:"success",
+                        customClass:{confirmButton:"btn btn-primary"},
+                        buttonsStyling:!1
+                      })
+
+
+                 }
+                },error: function (data) {
+                  var errors = data.responseJSON;
+                  $.each( errors.errors, function( key, value ) {
+                    toastr.error(value);
+                    return false;
+                  });
+                  //$('input').iCheck('enable');
+                  $('#formModulos input, #main-form button').removeAttr('disabled');
+                  $('#ajax-icon').removeClass('fas fa-spin fa-sync-alt').addClass('far fa-save');
+                }
+             });
+
+
+         }
+         else
+         {
+           return false;
+         }
+      });
+  </script>
+
+  <script>
+    $(document).on("click", ".btnBorrar", function(e){
+      e.preventDefault();
+      fila = $(this);
+      user_id = parseInt($(this).closest('tr').find('td:eq(0)').text()) ;
+      opcion = 3; //eliminar
+      Swal.fire({
+          title: '¿Estás seguro(a)?',
+          text: "¡Si confirmas no habrá marcha atrás!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonText: '¡Eliminar!',
+          customClass: {
+            confirmButton: 'btn btn-primary',
+            cancelButton: 'btn btn-outline-danger ms-1'
+          },
+          buttonsStyling: false
+        }).then(function (result) {
+          if (result.value) {
+          $.ajax({
+            url: "/users/"+user_id+'/delete' ,
+            type: "GET",
+            datatype:"json",
+
+            success: function() {
+                tablaModulos.row(fila.parents('tr')).remove().draw();
+                 var timerInterval;
+                    Swal.fire({
+                      title: '¡Datos eliminados!',
+                      icon: 'success',
+                      timer: 1000,
+                      timerProgressBar: false,
+                      didOpen: () => {
+                        Swal.showLoading();
+                        timerInterval = setInterval(() => {
+                          const content = Swal.getHtmlContainer();
+                          if (content) {
+                            const b = content.querySelector('b');
+                            if (b) {
+                              b.textContent = Swal.getTimerLeft();
+                            }
+                          }
+                        }, 100);
+                      },
+                      willClose: () => {
+                        clearInterval(timerInterval);
+                      }
+                    }).then(result => {
+                      /* Read more about handling dismissals below */
+                      if (result.dismiss === Swal.DismissReason.timer) {
+                        console.log('I was closed by the timer');
+                      }
+                    });
+             }
+          });
+
+          }
+        });
+    });
   </script>
 @endpush
