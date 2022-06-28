281d02e3544fda19ce1bd0f2ab1d5bcd5eb3e36a
richard viteri <reviteri@bandes.gob.ve>
2022-06-28 11:46:19 -0400

    modulo empleados vistas

---
+++ app/Models/Movimientos.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movimientos extends Model
+{
+    use HasFactory;
+
+    protected $table = "covid.movimientos";
+
+   	//public $timestamps = false;
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['id' ,'id_tipo_prueba', 'desde', 'hasta', 'resultado', 'id_personal','observacion','id_tipo_visita','id_user','id_estatus'];
+
+	protected $hidden = ['created_at', 'updated_at'];
+
+	public function obtenerMovimientos()
+	{
+	    return Movimientos::orderBy('id', 'DESC')->get();
+	}
+
+	public function obtenerMovimientosId($id)
+	{
+	    return Movimientos::find($id);
+	}
+}
